d9fb42782d60e79fa9b893460939e63162cbec84
Eva Bergold <evabergold@gmail.com>
2024-04-10 17:15:58 +0200

    worked on JS inputs and strings challenges

--- Javascript/js-inputs-and-strings_multiple-inputs/js/index.js
+++ Javascript/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,32 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+//1. On button click: The value of the first input field is copied into the second input field
+
+button.addEventListener("click", () => {
+
+    secondInput.value = firstInput.value;
+    
+});
+
+
+// 2. On button click: The value of the first input field is copied in uppercase into the second input field
+
+// button.addEventListener("click", () => {
+
+    
+//     const upperValue = firstInput.value;
+//     secondInput.value =upperValue.toUpperCase();
+
+//     });
+
+//3. On button click: The values of the two input fields switch.
+    
+// button.addEventListener("click", () => {
+
+//     const oldInput =firstInput.value;
+//     firstInput.value = secondInput.value;
+//     secondInput.value= oldInput;
+
+// });
