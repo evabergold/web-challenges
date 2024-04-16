15ff12974db0f14a47ab273c5dbb71a70a430fb6
Eva Bergold <evabergold@gmail.com>
2024-04-16 21:41:27 +0200

    work on js challenges

--- Javascript/js-structure_spacerocket/js/launchSequence.js
+++ Javascript/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,18 @@
 // Implement the launch sequence function here and export it as the default export.
+
+import { loadPayload } from "./core/load.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+
+function launchSequence (){
+
+    loadPayload(NFSAT);
+    loadPayload(FISHSAT);
+    fuel();
+    
+
+}
+
+
