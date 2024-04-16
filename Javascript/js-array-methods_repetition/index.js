115662b1e944ebc1070eaef3f25658e0964647cf
Eva Bergold <evabergold@gmail.com>
2024-04-16 21:26:08 +0200

    solved some js challenges

--- Javascript/js-array-methods_repetition/index.js
+++ Javascript/js-array-methods_repetition/index.js
@@ -36,7 +36,7 @@ const recipes = [
 */
 
 const onlyTitles = recipes.map((recipe) => recipe.title); // ['Crepes', ...]
-console.log(onlyTitles);
+console.log("Only Titles: " + onlyTitles);
 
 const titlesWithDuration = recipes.map((recipe) => {
   if (recipe.duration < 1440) {
@@ -45,22 +45,21 @@ const titlesWithDuration = recipes.map((recipe) => {
     return recipe.title + " " + recipe.duration / 24 / 60 + " days";
   }
 }); // ['Crepes (60min)', ...]
-console.log(titlesWithDuration);
+console.log("Titles with duration: " + titlesWithDuration);
 
 const timePerServing = recipes.map((recipe) => {
   return recipe.duration / recipe.servings;
 }); // [20, 10, 432, 12]
 
-console.log(timePerServing);
+console.log("Time per Serving: " + timePerServing);
 
 // EXTRA:
 
 // HINT: use first map() and then Array.prototype.join() with "method chaining" like so:
 // myArray.map(...).join(', ');
 
-const allTitlesInOneString = recipes.map(recipe =>  recipe.title).join(', ');
-console.log("String : '" + allTitlesInOneString + "'");
-
+const allTitlesInOneString = recipes.map((recipe) => recipe.title).join(", ");
+console.log("All titles in one string : '" + allTitlesInOneString + "'");
 
 // 'Crepes, Scrambled Eggs, ...'
 
@@ -71,12 +70,26 @@ console.log("String : '" + allTitlesInOneString + "'");
  2: `filter` exercises
 */
 
-const recipesThatOnlyTake60minutesOrLess = recipes.filter((recipe)=> recipe.duration < 60);
-console.log ("Recipes that take 60 minutes or less: " +  recipesThatOnlyTake60minutesOrLess);
+const recipesThatOnlyTake60minutesOrLess = recipes.filter((recipe) => {
+  return recipe.duration <= 60;
+});
+console.log("Recipes that only take 60 minutes or less:")
+console.log(recipesThatOnlyTake60minutesOrLess);
 
-const allRecipesWithMoreThan2Servings = null;
+const allRecipesWithMoreThan2Servings = recipes.filter((recipe) => {
+  return recipe.servings >= 3;
+});
+console.log("All recipes with more than 2 servings:")
+console.log(allRecipesWithMoreThan2Servings);
 
-const allRecipesWithTitlesLongerThan12Characters = null;
+const allRecipesWithTitlesLongerThan12Characters = recipes.filter((recipe)=> {
+
+  return recipe.title.length > 12;
+
+});
+
+console.log("All recipes with titles longer than 2 characters: ")
+console.log (allRecipesWithTitlesLongerThan12Characters);
 
 export {
   onlyTitles,
