7ad0c1cf3889ea12ffcc59e7711e33f1db1ebc61
Eva Bergold <evabergold@gmail.com>
2024-04-17 08:51:48 +0200

    work on js structure challenges

--- Javascript/js-structure_spacerocket/js/launchSequence.js
+++ Javascript/js-structure_spacerocket/js/launchSequence.js
@@ -5,13 +5,23 @@ import { fuel } from "./core/fuel.js";
 import { countdown } from "./core/countdown.js";
 import { liftoff } from "./core/liftoff.js";
 import { deployPayload } from "./core/deploy.js";
+import { NFSAT } from "./payload/satellites.js";
+import { FISHSAT} from "./payload/satellites.js";
 
-function launchSequence (){
+
+
+export default function launch() {
 
     loadPayload(NFSAT);
     loadPayload(FISHSAT);
     fuel();
-    
+    countdown();
+    countdown();
+    countdown();
+    countdown();
+    countdown();
+    liftoff();
+    deployPayload();
 
 }
 
