cf826b7526ee4d6bfa49830c98b18c4bd62c8a52
Eva Bergold <evabergold@gmail.com>
2024-04-17 10:07:09 +0200

    optimize js structure quiz app challenge

--- Javascript/js-structure_quiz-app/index.js
+++ Javascript/js-structure_quiz-app/index.js
@@ -1,11 +1,4 @@
 import { App } from "./components/App/App.js";
-import { Bookmark } from "./components/Bookmark/Bookmark.js";
-import { Card } from "./components/Card/Card.js";
-import { CardList } from "./components/CardList/CardList.js";
-import { Form } from "./components/Form/Form.js";
-import { handleFormSubmit } from "./components/Form/Form.js";
-import { Header } from "./components/Header/Header.js";
-import { cards } from "./components/Card/Card.js";
 
 
 
