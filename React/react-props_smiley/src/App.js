afb297afc6d79569c66f6131404614ba84941f5b
Eva Bergold <evabergold@gmail.com>
2024-04-23 17:15:44 +0200

    work on react challenge

--- React/react-props_smiley/src/App.js
+++ React/react-props_smiley/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  const isHappy = true;
+  //const isHappy = true;
   return (
     <h1>
-      <Smily isHappy={isHappy} />
+      <Smily isHappy />
     </h1>
   );
 }
 
 function Smily({ isHappy }) {
-  return <>{isHappy ? <div>:-)</div> : <div>:-(</div>}</>;
+  // return <>{isHappy ? <div>:-)</div> : <div>:-(</div>}</>;
+  const smily = isHappy ? ":-)" : ":-(";
+  return <div>{smily}</div>;
 }
