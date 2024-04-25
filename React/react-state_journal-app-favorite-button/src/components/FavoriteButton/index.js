705b8d861e98948c76f53015f6aeed2f45e82749
Eva Bergold <evabergold@gmail.com>
2024-04-25 15:00:41 +0200

    optimize react challenge

--- React/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ React/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -7,13 +7,15 @@ export default function FavoriteButton() {
   // this should be a state variable
   const [isFavorite, setIsFavorite] = useState(false);
 
+  function handleToggleFavorite() {
+    setIsFavorite(!isFavorite);
+    console.log("favorite button clicked");
+  }
+
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        setIsFavorite(!isFavorite);
-        console.log("favorite button clicked");
-      }}
+      onClick={handleToggleFavorite}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
